c289ad7dbc53c410aeef04172bf2a14c57393670
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-07-02 10:33:21 -0300

    Feat: create types to months and update eslint config file

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -5,30 +5,52 @@ import { AgendaContainer, AgendaInput, AgendaTitle } from "./styles";
 export const Schedule: React.FC = () => {
 
   type AgendaProps = {
-    day: string,
+    day: number,
     month: string,
     year: string
   }
 
-  const [agendaDate, setAgendaDate] = useState<AgendaProps>();
+  type monthProps = {
+      1: string,
+      2: string,
+      3: string,
+      4: string,
+      5: string,
+      6: string,
+      7: string,
+      8: string,
+      9: string,
+      10: string,
+      11: string,
+      12: string
+  }
+
+  const initialValues = {
+    day: 1,
+    month: 'January',
+    year: '2000'
+  }
+
+  const [agendaDate, setAgendaDate] = useState<AgendaProps>(initialValues)
 
   const handleMonth = (month: string): string => {
 
-    const monthObj = {
-      "01": "January",
-      "02": "February",
-      "03": "March",
-      "04": "April",
-      "05": "May",
-      "06": "June",
-      "07": "July",
-      "08": "August",
-      "09": "September",
-      "10": "October",
-      "11": "November",
-      "12": "December",
+    const monthObj: monthProps = {
+      1: "January",
+      2: "February",
+      3: "March",
+      4: "April",
+      5: "May",
+      6: "June",
+      7: "July",
+      8: "August",
+      9: "September",
+      10: "October",
+      11: "November",
+      12: "December"
     }
-      return monthObj[month]
+    // @ts-ignore
+     return monthObj[+month]
   }
 
   const handleAgendaDate = (date: string) => {
@@ -38,7 +60,7 @@ export const Schedule: React.FC = () => {
     const dateYear = date.split("-")[0]
 
     setAgendaDate({
-      day: dateDay,
+      day: +dateDay,
       month: handleMonth(dateMonth),
       year: dateYear
     })
@@ -48,7 +70,7 @@ export const Schedule: React.FC = () => {
   return (
     <AgendaContainer>
       <AgendaTitle>{ agendaDate.day } { agendaDate.month } { agendaDate.year }</AgendaTitle>
-      <AgendaInput type="date" onChange={(event: any) => handleAgendaDate(event.target.value)} />
+      <AgendaInput type="date" onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleAgendaDate(event.target.value)} />
     </AgendaContainer>
   );
 };
